47d0521a936ca46cd65632c30b32a599070c53e2
Prabhneet <prabhneet13@gmail.com>
2023-05-09 14:22:46 +0530

    Serializer Relation

--- API/serializers.py
+++ API/serializers.py
@@ -5,9 +5,16 @@ from .models import Singer, Song
 
 
 class SingerSerializer(serializers.ModelSerializer):
+
+    # It creates Hyperlink of the relation
+    # song = serializers.HyperlinkedIdentityField(many=True, read_only=True, view_name='song-detail')
+
+    # Serializer Relation
+    song = serializers.StringRelatedField(many=True, read_only=True)
+
     class Meta:
         model = Singer
-        fields = ['id', 'name', 'gender']
+        fields = ['id', 'name', 'gender', 'song']
 
 
 class SongSerializer(serializers.ModelSerializer):
