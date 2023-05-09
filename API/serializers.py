34ad88dd46a06a1d1e65c0f23bb063088619d1d0
Prabhneet <prabhneet13@gmail.com>
2023-05-09 14:29:25 +0530

    Nested Serializer

--- API/serializers.py
+++ API/serializers.py
@@ -4,20 +4,23 @@ from .models import Singer, Song
 # Model Serializer
 
 
+class SongSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Song
+        fields = ['id', 'title', 'singer', 'duration']
+
+
 class SingerSerializer(serializers.ModelSerializer):
 
     # It creates Hyperlink of the relation
     # song = serializers.HyperlinkedIdentityField(many=True, read_only=True, view_name='song-detail')
 
     # Serializer Relation
-    song = serializers.StringRelatedField(many=True, read_only=True)
+    # song = serializers.StringRelatedField(many=True, read_only=True)
+
+    # Nested Serializer
+    song = SongSerializer(many=True, read_only=True)
 
     class Meta:
         model = Singer
         fields = ['id', 'name', 'gender', 'song']
-
-
-class SongSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Song
-        fields = ['id', 'title', 'singer', 'duration']
